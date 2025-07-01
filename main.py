6301c4780e81e7343609737db582d67c881bf2e4
AK-Git508 <aariz.mkgb@gmail.com>
2025-07-01 17:49:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ col1,col2,col3, = st.columns([1,2,1])
 #   Divison Checker
 
 with col2:
-dividend = int(st.text_input("Enter dividend here: "))
-divisor = int(st.text_input("Enter divisor here: "))
+  dividend = int(st.text_input("Enter dividend here: "))
+  divisor = int(st.text_input("Enter divisor here: "))
 
 try:
   st.write (divident/divisor)
 except ZeroDivisionError
   st.write ("Divsor cannot be zero")
- 
+  
