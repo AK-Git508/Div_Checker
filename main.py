1d33ae333435fd1b5ae31bb5490e43c926e85276
AK-Git508 <aariz.mkgb@gmail.com>
2025-07-01 18:01:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import math
 
-col1,col2,col3, = st.columns([1,2,1])
 
-#   Divison Checker
+col1, col2, col3 = st.columns([1, 2, 1]) 
 
 with col2:
-  dividend = int(st.text_input("Enter dividend here: ",value = 5))
-  divisor = int(st.text_input("Enter divisor here: ",value = 5))
+  st.title("Div Checker")
+  dividend=int(st.text_input("Enter your dividend here: ", value=5))
+  divisor=int(st.text_input("Enter your divisor here: ", value=5))
 
 try:
-  st.write (dividend/divisor)
+  st.write(dividend/divisor)
 except ZeroDivisionError:
-  st.write ("Divsor cannot be zero")
-  
+  st.write("The divisor cannot be zero!")
+except ValueError:
+  st.write("Make sure to enter in two numbers!")
