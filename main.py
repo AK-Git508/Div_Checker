e9ead8d97493c84ade9632fe9a3ad2501c29656e
AK-Git508 <aariz.mkgb@gmail.com>
2025-07-01 17:56:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ col1,col2,col3, = st.columns([1,2,1])
 #   Divison Checker
 
 with col2:
-  dividend = int(st.text_input("Enter dividend here: "))
-  divisor = int(st.text_input("Enter divisor here: "))
+  dividend = int(st.text_input("Enter dividend here: ",value))
+  divisor = int(st.text_input("Enter divisor here: ",value))
 
 try:
   st.write (divident/divisor)
